a57ab9b4cefccc97739d8f9e10bb0ae1abe10ef2
HarrisLich <112398044+HarrisLich@users.noreply.github.com>
2022-09-07 21:32:25 -0400

    Add files via upload

---
+++ assignment2/if_else_input_practice.py
@@ -0,0 +1,32 @@
+
+#These instructions below are copied fromt the other example, the adjustments are a space after
+#Instructions: Create a variable of any name and set it equal to 10.
+#The first if statement should read: if 10 (but use our variable, not the number 10) is greater than 12, print out "10 is greater than 12"
+#The second if/else should read: Else if 10 is greater than 11, print out "10 is greater than 11"
+#The third if/else should read: Else if 10 is equal to 10, print out "10 is equal to 10"
+#The else should read: Else print out "10 is less than 10"
+
+
+#additional instructions: instead of setting our variable to 10, what if we asked the user for the variable and used that value in our comparisons?
+def main():
+
+    input1 = input("Pick a number...")
+
+    if input1.isdigit():
+        num = int(input1)
+        if num > 12:
+            print(str(num), " is greater than 12")
+        elif num > 11:
+            print(str(num), " is greater than 11")
+        elif num == 10:
+            print(str(num), " is equal to 10")
+        else:
+            print(str(num), " is less than 10")
+    else:
+        print("please choose an integer")
+
+
+
+while(True):
+    main()
+

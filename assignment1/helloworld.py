29825c8aa2a22f2a8f88dbd6090a1d07678adec1
HarrisLich <112398044+HarrisLich@users.noreply.github.com>
2022-09-07 21:31:27 -0400

    Add files via upload

---
+++ assignment1/helloworld.py
@@ -0,0 +1,24 @@
+#Harris Lichstein
+#CMPT 120
+#9/1/22
+
+# instructions: print out "Hello World!"
+# create a variable to hold your favorite color, then print out that variable with a string beforehand that says "My favorite color is: "
+# create a variable to hold your birthday (NO SLASHES, JUST THE DIGITS) and then print out "My birthday is: " with the variable
+
+
+color = "Purple"
+date = 2182004
+
+def main():
+    print("Hello World!")
+    print("My Favorite Color is: " + color)
+    print("My birthday is: " + str(date))
+
+
+
+
+
+
+
+main()
